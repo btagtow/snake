23f5396b34aac66e7fb3f7f9ce4fd1c4e116bf03
Benjamin Tagtow <benjamintagtow@Benjamins-MacBook-Air.local>
2020-12-02 12:49:24 -0700

    small commit - began creating grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    rows: 10,
+    columns: 10,
+    grid: [],
+  };
+  
 
+  render (){
+    return (
+    <div className="App">
+      <h1 className="head">Snake</h1>
+      <div className="snake-container">
+        <div className="grid-box" > </div>
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
